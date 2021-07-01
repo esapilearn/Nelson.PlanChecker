c79632e828dca15faa31602cd8639d39e1119330
Kristin Nelson <kjeastman@ymail.com>
2021-07-01 10:23:17 -0700

    Added unit test to CTDate constraint

---
+++ ESAPIX WPF ExampleTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Constraints;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void ConstrainTest()
+        {
+            //Arrange
+            var ctDate = DateTime.Now.AddDays(-61);
+            //Act
+            var actual = new CTDateConstraint().Constrain(ctDate).ResultType;
+            //Assert
+            var expected = ResultType.ACTION_LEVEL_3;
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
